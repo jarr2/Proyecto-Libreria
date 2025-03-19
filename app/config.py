01871b49e914ab91741f252a43f22905c4672cc6
Cristobal Eduardo Serrano Bahena <20223TN111@utez.edu.mx>
2025-03-18 22:11:24 -0600

    Si fue el Arturo el que madreo mi carrito

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class DevelopmentConfig():
     SECRET_KEY = os.urandom(24)
     DEBUG = True
     MYSQL_DATABASE_USER = 'root'
-    MYSQL_DATABASE_PASSWORD = 'root!'
+    MYSQL_DATABASE_PASSWORD = 'root'
     MYSQL_DATABASE_HOST = '127.0.0.1'
     MYSQL_DATABASE_PORT = 3306
     MYSQL_DATABASE_DB = 'LibOnline'
